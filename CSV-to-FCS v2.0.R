b8901e4af11aca11183a68c53d2bf991230fefa0
Christian Rickert <rc.login@icloud.com>
2022-03-31 15:21:07 -0600

    code refactoring

--- CSV-to-FCS v2.0.R
+++ CSV-to-FCS v2.0.R
@@ -1,104 +1,49 @@
 # CSV to FCS
-    # Coverting .csv file data into an .fcs file
-    # Thomas Ashhurst
-    # 2017-09-13
-    # github.com/sydneycytometry
-    # .fcs file reading and writing adapted from https://gist.github.com/yannabraham/c1f9de9b23fb94105ca5
+    # Converting .csv file data into an .fcs file
+    # Thomas Ashhurst (2017-09-13) [github.com/sydneycytometry]
+    # *adapted from [https://gist.github.com/yannabraham/c1f9de9b23fb94105ca5]
 
-    # Package installation updated, Christian Rickert (2021-03-10)
-    # Manual path entry updated, Christian (2021-08-29)
+# Code refactoring by Christian Rickert (2022-03-31)
 
-##### USER INPUT #####
+# Install required packages
+if(!requireNamespace("BiocManager", quietly = TRUE)) {install.packages("BiocManager", quiet = TRUE)}
+if(!require('flowCore')) {BiocManager::install("flowCore", update = FALSE)}
+if(!require('Biobase')) {BiocManager::install("Biobase", update = FALSE)}
+if(!require('data.table')) {install.packages('data.table')}
+if(!require('rstudioapi')) {install.packages('rstudioapi')}
 
-    # Install packages if required
-    rver = R.version
-    if (rver$major < 3 || (rver$major == 3 && rver.minor <= 5)){
-      # keep backwards compatibility for older R versions
-      if(!require('flowCore')) {install.packages('flowCore')}
-      if(!require('Biobase')) {install.packages('Biobase')}
-    } else {
-      # using BiocManager for future compatibility with R
-      if(!requireNamespace("BiocManager", quietly = TRUE))
-      {
-        # there's usually no need to install from source, install remote packages instead
-        install.packages("BiocManager", quiet = TRUE)
-        # the update argument suppresses BiocManager's automated update dialog prompt
-        if(!require('flowCore')) {BiocManager::install("flowCore", update = FALSE)}
-        if(!require('Biobase')) {BiocManager::install("Biobase", update = FALSE)}
-      }
-    }
-    if(!require('data.table')) {install.packages('data.table')}
-    if(!require('rstudioapi')) {install.packages('rstudioapi')}
+# Load packages
+library('flowCore')
+library('Biobase')
+library('data.table')
+library('rstudioapi')
 
-    # Load packages
-    library('flowCore')
-    library('Biobase')
-    library('data.table')
+# Set input and output variables
+currentFolder <- dirname(rstudioapi::getSourceEditorContext()$path)  # script location
+importFolder <- file.path(currentFolder, "import", fsep=.Platform$file.sep)  # relative path
+exportFolder <- file.path(currentFolder, "export", fsep=.Platform$file.sep)
+filePattern  <- ".csv"
 
-    # In order for this to work, a) rstudioapi must be installed and b) the location of this .r script must be in your desired working directory
-    #dirname(rstudioapi::getActiveDocumentContext()$path)            # Finds the directory where this script is located
-    #setwd(dirname(rstudioapi::getActiveDocumentContext()$path))     # Sets the working directory to where the script is located
-    #getwd()
-    #PrimaryDirectory <- getwd()
-    #PrimaryDirectory
-    
-    # Use this to manually set the working directory
-    WorkingDirectory <- file.path("D:", "foo bar", "..", "name", fsep=.Platform$file.sep)  # Works across platforms, e.g. "D:\\foo\ bar\\..\\name" on Windows
-    setwd(WorkingDirectory)                                         # Set your working directory here -- press tab when selected after the '/' to see options
-    getwd()                                                         # Check your working directory has changed correctly
-    PrimaryDirectory <- getwd()                                     # Assign the working directory as 'PrimaryDirectory'
-    PrimaryDirectory
+# Set up working directories
+if (!file.exists(importFolder)) {dir.create(importFolder)}
+print(paste("Input folder: ", importFolder))
+if (!file.exists(exportFolder)) {dir.create(exportFolder)}
+print(paste("Export folder: ", exportFolder))
 
-    ## Use to list the .csv files in the working directory -- important, the only CSV files in the directory should be the one desired for analysis. If more than one are found, only the first file will be used
-    FileNames <- list.files(path=PrimaryDirectory, pattern = ".csv")     # see a list of CSV files
-    as.matrix(FileNames) # See file names in a list
-    
-    ## Read data from Files into list of data frames
-    DataList=list() # Creates and empty list to start 
+# Get list of input files
+FileNames <- list.files(path = importFolder, pattern = filePattern)
 
-    for (File in FileNames) { # Loop to read files into the list
-      tempdata <- fread(File, check.names = FALSE)
-      File <- gsub(".csv", "", File)
-      DataList[[File]] <- tempdata
-    }
+# Read data from CSV and write FCS data
+for (File in FileNames) {
+  print(File)
 
-    rm(tempdata)
-    AllSampleNames <- names(DataList)
-    
-    ## Chech data quality
-    head(DataList)
+  setwd(importFolder)
+  FileData <- fread(file=File, check.names = FALSE)
+  metadata <- data.frame(name=dimnames(FileData)[[2]],desc=paste('column',dimnames(FileData)[[2]],'from dataset'))
+  metadata$minRange <- apply(FileData,2,min)
+  metadata$maxRange <- apply(FileData,2,max)
 
-    
-    
-##### END USER INPUT #####
-
-    x <- Sys.time()
-    x <- gsub(":", "-", x)
-    x <- gsub(" ", "_", x)
-
-    newdir <- paste0("Output_CSV-to-FCS", "_", x)
-    
-    setwd(PrimaryDirectory)
-    dir.create(paste0(newdir), showWarnings = FALSE)
-    setwd(newdir)
-    
-    
-    for(i in c(1:length(AllSampleNames))){
-      data_subset <- DataList[i]
-      data_subset <- rbindlist(as.list(data_subset))
-      dim(data_subset)
-      a <- names(DataList)[i]
-
-      metadata <- data.frame(name=dimnames(data_subset)[[2]],desc=paste('column',dimnames(data_subset)[[2]],'from dataset'))
-      
-      ## Create FCS file metadata - ranges, min, and max settings
-      #metadata$range <- apply(apply(data_subset,2,range),2,diff)
-      metadata$minRange <- apply(data_subset,2,min)
-      metadata$maxRange <- apply(data_subset,2,max)
-      
-      data_subset.ff <- new("flowFrame",exprs=as.matrix(data_subset), parameters=AnnotatedDataFrame(metadata)) # in order to create a flow frame, data needs to be read as matrix by exprs
-      head(data_subset.ff)
-      write.FCS(data_subset.ff, paste0(a, ".fcs"))
-    }
-
-    
+  setwd(exportFolder)
+  FileData.ff <- new("flowFrame",exprs=as.matrix(FileData), parameters=AnnotatedDataFrame(metadata)) # in order to create a flow frame, data needs to be read as matrix by exprs
+  write.FCS(FileData.ff, paste0(File, ".fcs"))
+}
--- FCS-to-CSV v2.0.R
+++ FCS-to-CSV v2.0.R
@@ -1,97 +1,46 @@
 # FCS to CSV
-    # Coverting .fcs file data into an .csv file
-    # Thomas Ashhurst
-    # 2017-09-13
-    # github.com/sydneycytometry
-    # .fcs file reading and writing adapted from https://gist.github.com/yannabraham/c1f9de9b23fb94105ca5
+    # Converting .fcs file data into an .csv file
+    # Thomas Ashhurst (2017-09-13) [github.com/sydneycytometry]
+    # *adapted from [https://gist.github.com/yannabraham/c1f9de9b23fb94105ca5]
 
-    # Package installation updated, Christian Rickert (2021-03-10)
-    # Manual path entry updated, Christian (2021-08-29)
+# Code refactoring by Christian Rickert (2022-03-31)
 
-##### USER INPUT #####
-    
-    # Install packages if required
-    rver = R.version
-    if (rver$major < 3 || (rver$major == 3 && rver.minor <= 5)){
-      # keep backwards compatibility for older R versions
-      if(!require('flowCore')) {install.packages('flowCore')}
-      if(!require('Biobase')) {install.packages('Biobase')}
-    } else {
-      # using BiocManager for future compatibility with R
-      if(!requireNamespace("BiocManager", quietly = TRUE))
-      {
-        # there's usually no need to install from source, install remote packages instead
-        install.packages("BiocManager", quiet = TRUE)
-        # the update argument suppresses BiocManager's automated update dialog prompt
-        if(!require('flowCore')) {BiocManager::install("flowCore", update = FALSE)}
-        if(!require('Biobase')) {BiocManager::install("Biobase", update = FALSE)}
-      }
-    }
-    if(!require('data.table')) {install.packages('data.table')}
-    if(!require('rstudioapi')) {install.packages('rstudioapi')}
-    
-    # Load packages
-    library('flowCore')
-    library('Biobase')
-    library('data.table')
-    
-    # In order for this to work, a) rstudioapi must be installed and b) the location of this .r script must be in your desired working directory
-    #dirname(rstudioapi::getActiveDocumentContext()$path)            # Finds the directory where this script is located
-    #setwd(dirname(rstudioapi::getActiveDocumentContext()$path))     # Sets the working directory to where the script is located
-    #getwd()
-    #PrimaryDirectory <- getwd()
-    #PrimaryDirectory
-    
-    # Use this to manually set the working directory
-    WorkingDirectory <- file.path("D:", "foo bar", "..", "name", fsep=.Platform$file.sep)  # Works across platforms, e.g. "D:\\foo\ bar\\..\\name" on Windows
-    setwd(WorkingDirectory)                                         # Set your working directory here -- press tab when selected after the '/' to see options
-    getwd()                                                         # Check your working directory has changed correctly
-    PrimaryDirectory <- getwd()                                     # Assign the working directory as 'PrimaryDirectory'
-    PrimaryDirectory
-    
-    ## Use to list the .csv files in the working directory -- important, the only CSV files in the directory should be the one desired for analysis. If more than one are found, only the first file will be used
-    FileNames <- list.files(path=PrimaryDirectory, pattern = ".fcs")     # see a list of CSV files
-    as.matrix(FileNames) # See file names in a list
-    
-    ## Read data from Files into list of data frames
-    DataList=list() # Creates and empty list to start 
+# Install required packages
+if(!requireNamespace("BiocManager", quietly = TRUE)) {install.packages("BiocManager", quiet = TRUE)}
+if(!require('flowCore')) {BiocManager::install("flowCore", update = FALSE)}
+if(!require('Biobase')) {BiocManager::install("Biobase", update = FALSE)}
+if(!require('data.table')) {install.packages('data.table')}
+if(!require('rstudioapi')) {install.packages('rstudioapi')}
 
-    for (File in FileNames) { # Loop to read files into the list
-      tempdata <- exprs(read.FCS(File, transformation = FALSE))
-      tempdata <- tempdata[1:nrow(tempdata),1:ncol(tempdata)]
-      File <- gsub(".fcs", "", File)
-      DataList[[File]] <- tempdata
-    }
+# Load packages
+library('flowCore')
+library('Biobase')
+library('data.table')
+library('rstudioapi')
 
-    rm(tempdata)
-    AllSampleNames <- names(DataList)
-    
-    ## Chech data quality
-    head(DataList)
+# Set input and output variables
+currentFolder <- dirname(rstudioapi::getSourceEditorContext()$path)  # script location
+importFolder <- file.path(currentFolder, "export", fsep=.Platform$file.sep)  # relative path
+exportFolder <- file.path(currentFolder, "import2", fsep=.Platform$file.sep)
+filePattern  <- ".fcs"
 
-    
-    
-##### END USER INPUT #####
-    
-    x <- Sys.time()
-    x <- gsub(":", "-", x)
-    x <- gsub(" ", "_", x)
-    
-    newdir <- paste0("Output_FCS-to-CSV", "_", x)
-    
-    setwd(PrimaryDirectory)
-    dir.create(paste0(newdir), showWarnings = FALSE)
-    setwd(newdir)
-    
-    
-    for(i in c(1:length(AllSampleNames))){
-      data_subset <- DataList[i][[1]]
-      data_subset <- as.data.frame(data_subset)
-      colnames(data_subset)
-      #data_subset <- rbindlist(as.list(data_subset$sample_data))
-      dim(data_subset)
-      a <- names(DataList)[i]
+# Set up working directories
+if (!file.exists(importFolder)) {dir.create(importFolder)}
+print(paste("Input folder: ", importFolder))
+if (!file.exists(exportFolder)) {dir.create(exportFolder)}
+print(paste("Export folder: ", exportFolder))
 
-      write.csv(data_subset, paste0(a, ".csv"))
-    }
-    
+# Get list of input files
+FileNames <- list.files(path = importFolder, pattern = filePattern)
+
+# Read data from CSV and write FCS data
+for (FileName in FileNames) {
+  print(FileName)
+  
+  setwd(importFolder)
+  FileData <- exprs(read.FCS(FileName, transformation = FALSE))
+  FileData <- FileData[1:nrow(FileData),1:ncol(FileData)]
+
+  setwd(exportFolder)
+  write.csv(FileData, paste0(FileName, ".csv"))
+}
